5460db2c34abe7538a79ff698a6fbdf10c043169
Pawan Singh Bora <pawansinghb@audaxlabs.com>
2024-12-16 14:39:59 +0530

    add toggle term

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -25,3 +25,26 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+local buf_id = nil;
+local win_id = nil;
+vim.keymap.set("n", "<leader>t", function ()
+  if win_id==nil or not vim.api.nvim_win_is_valid(win_id) then
+    local buf_exit = true;
+    if(buf_id==nil or not vim.api.nvim_buf_is_valid(buf_id)) then
+      buf_id =  vim.api.nvim_create_buf(false, true);
+      buf_exit = false;
+    end
+     win_id = vim.api.nvim_open_win(buf_id, true, {
+      split = "below",
+      win = 0,
+      height = 8,
+    })
+    if(not buf_exit) then
+      vim.cmd.term();
+    end
+  else
+    vim.api.nvim_win_close(win_id, true);
+    win_id = nil;
+  end
+end)
