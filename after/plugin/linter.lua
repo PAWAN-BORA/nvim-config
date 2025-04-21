948500c8c8766882af8c94cf01cc116da3e26a5b
Pawan Singh Bora <pawansinghb@audaxlabs.com>
2025-04-21 15:21:44 +0530

    run eslit if linter is setup on the project

--- after/plugin/linter.lua
+++ after/plugin/linter.lua
@@ -4,14 +4,20 @@ local lint = require('lint')
 lint.linters_by_ft = {
   javascript = { "eslint_d" },
   typescript = { "eslint_d" },
-  python = { "pylint" },
-  lua = { "luacheck" },
-  go = { "golangcilint" },
-  sh = { "shellcheck" },
+  -- python = { "pylint" },
+  -- lua = { "luacheck" },
+  -- go = { "golangcilint" },
+  -- sh = { "shellcheck" },
 }
 
+
+local function isEslintSetup()
+  return vim.fn.filereadable('.eslintrc.js') ==1 or vim.fn.filereadable('.eslintrc.json') == 1 or vim.fn.filereadable('eslint.config.js') == 1
+end
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
   callback = function()
-    lint.try_lint()
+    if isEslintSetup() then
+      lint.try_lint()
+    end
   end,
 })
