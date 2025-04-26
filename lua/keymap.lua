a4b50e0bebc9dac161086cf80a4e66296daf063e
Pawan <funwithcodings@gamil.com>
2025-04-26 15:23:52 +0530

    add lsp plugins

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,16 +1,37 @@
-vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+local keymap = vim.keymap;
+keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("v", "<leader>s", [[:s/\<<C-r>"\>/<C-r>"/gI<Left><Left><Left>]])
+keymap.set("x", "<leader>p", "\"_dP")
+
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("v", "<leader>s", [[:s/\<<C-r>"\>/<C-r>"/gI<Left><Left><Left>]])
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    local lsp = vim.lsp;
+    keymap.set('n', 'K', lsp.buf.hover, opts)
+    keymap.set('n', 'gd', lsp.buf.definition, opts)
+    keymap.set('n', 'gD', lsp.buf.declaration, opts)
+    keymap.set('n', 'gi', lsp.buf.implementation, opts)
+    keymap.set('n', 'go', lsp.buf.type_definition, opts)
+    keymap.set('n', 'gr', lsp.buf.references, opts)
+    keymap.set('n', 'gs', lsp.buf.signature_help, opts)
+    keymap.set('n', '<F2>', lsp.buf.rename, opts)
+    keymap.set('n', '<leader>i', vim.diagnostic.open_float, opts)
+    -- keymap.set({ 'n', 'x' }, '<F3>', lsp.buf.format({ async = true }), opts)
+    keymap.set('n', '<F4>', lsp.buf.code_action, opts)
+  end
+})
